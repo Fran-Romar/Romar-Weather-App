6519cf8947024e510abd6b5697efbd92b1234f1f
Fran-Romar <franromar2002@mail.com>
2023-10-10 13:52:07 +0200

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./css/weather.css";
+import "../css/weather.css";
 import WeatherApp from "./WeatherApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
